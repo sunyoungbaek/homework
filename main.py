844016348f96cd5fcd9658e52b1c400e1f5359b8
sunyoungbaek <sunyoung.baek327@gmail.com>
2019-07-27 12:09:06 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import sys
-from homework import wordCounts
-from homework import wordProbs
+from word_counts import wordCounts
+from word_probs import wordProbs
 
 if __name__== "__main__":
   wordProbs.getUniBigramWeights(sys.argv[1])  
