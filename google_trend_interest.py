e5999a044c9caadd204c9f849a46dcaad1daa780
sunyoungbaek <sunyoung.baek327@gmail.com>
2019-07-27 13:42:13 -0700

    Update google_trend_interest.py

--- google_trend_interest.py
+++ google_trend_interest.py
@@ -1,21 +1,20 @@
 import sys
 from pytrends.request import TrendReq
 
-def googleTrendInterest(query):
-    words = query.lower().split()
-    
+def googleTrendInterest(words):
     pytrend = TrendReq(hl='en-US', geo='US')
-    pytrend.build_payload([words[0], words[1], query], cat=0, timeframe='today 5-y')
+    pytrend.build_payload([words[0], words[1], words[0] + ' ' + words[1]], cat=0, timeframe='today 5-y')
     result = pytrend.interest_over_time()
     result = result.iloc[:, :3].sum()   
     
     
 if __name__== "__main__":
     query = sys.argv[1]
+    words = query.lower().split()
     
     # If query is not a bigram, return message
     if len(words) != 2:
-        return 'this word is not a bigram'
-    
-    df = googleTrendInterest(query)
-    sys.stdout.write(str(dict(result)))
+        sys.stdout.write('this word is not a bigram')
+    else:
+        df = googleTrendInterest(words)
+        sys.stdout.write(str(dict(result)))
